18d16c3e5bd2ece2d581ce5a6a5eb10129c5757b
R3nz0quispeaparicio <133979642+R3nz0quispeaparicio@users.noreply.github.com>
2024-07-20 17:23:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ if (isset($_POST['publicar'])) {
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="js/jquery.jscroll.js"></script>
     <style>
     .input-group-btn {
